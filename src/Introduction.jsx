1ccbe7cb37d777b96f7f1f81bc7e955403a6c520
enkeljon gjeta <enkelionimarku@gmail.com>
2024-10-05 20:15:22 +0200

    another try

--- src/Introduction.jsx
+++ src/Introduction.jsx
@@ -18,16 +18,18 @@ export default function Introduction()
                     <div className="bg-green-500 rounded-full h-40 w-40 mx-auto my-auto">
                     </div>
                     <div className="mx-10 my-10">
-                        <h1 className="font-body text-3xl font-bold">Enkeljon Gjeta</h1>
-                    : Code Maestro & Tech Explorer
-                    🚀 Second-year software engineering dynamo with a toolkit that packs a punch:
+                        <h1 className="font-body text-3xl font-bold text-center">Enkeljon Gjeta</h1>
+                        <h2 className="text-center">Code Maestro & Tech Explorer</h2>
+                        <p>
+                            🚀 Second-year software engineering dynamo with a toolkit that packs a punch:
+                            C, JavaScript, Python, C#, React? Check!
+                            IoT whiz kid? You bet! (Arduino & Raspberry Pi are my playground)
+                            Cybersecurity buff by day, frontend artist by night
 
-                    C, JavaScript, Python, C#, React? Check!
-                    IoT whiz kid? You bet! (Arduino & Raspberry Pi are my playground)
-                    Cybersecurity buff by day, frontend artist by night
-
-                    🏆 Hackathon warrior, RoboTech mentor, and full-stack adventurer
-                    From bits to bots, I'm not just coding the future – I'm engineering it. Ready to turn your next big idea into digital reality? Let's connect and create some tech magic! 💻✨
+                            🏆 Hackathon warrior, RoboTech mentor, and full-stack adventurer
+                            From bits to bots, I'm not just coding the future – I'm engineering it. Ready to turn your next big idea into digital reality? Let's connect and create some tech magic! 💻✨
+                        </p>
+                    
                     </div>
                 </div>
 
