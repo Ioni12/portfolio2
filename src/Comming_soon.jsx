20c3eff2139d801a0debb9fc5e99cc9882a4f200
enkeljon gjeta <enkelionimarku@gmail.com>
2024-10-26 13:01:23 +0200

    seeing end result of working on

--- src/Comming_soon.jsx
+++ src/Comming_soon.jsx
@@ -4,9 +4,9 @@ export default function Comming_soon()
 {
     return(
         <>
-            <div className="bg-green-400 h-[24rem] justify-center">
+            <div className="h-[24rem] justify-center">
                 currently working on
-                <div className="grid grid-cols-3 gap-2 bg-red-600 mt-10">
+                <div className="grid grid-cols-3 gap-2 mt-28">
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 ml-4"></div>
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mx-auto"></div>
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mr-"></div>
