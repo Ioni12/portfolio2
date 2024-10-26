983c15136cb08a844ef7988e3fd7cd5d53c9d5f7
enkeljon gjeta <enkelionimarku@gmail.com>
2024-10-26 12:57:57 +0200

    seeing the margins

--- src/Comming_soon.jsx
+++ src/Comming_soon.jsx
@@ -8,7 +8,7 @@ export default function Comming_soon()
                 currently working on
                 <div className="grid grid-cols-3 gap-2 bg-red-600 mt-10">
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 ml-4"></div>
-                    <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28"></div>
+                    <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mx-auto"></div>
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mr-"></div>
                 </div>
                 
