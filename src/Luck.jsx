977141d8de879b9520b1e7a77e0324ea72f9dff8
enkeljon gjeta <enkelionimarku@gmail.com>
2024-10-26 15:24:34 +0200

    another pseudo complete

--- src/Luck.jsx
+++ src/Luck.jsx
@@ -1,10 +1,11 @@
 import ScratchCard from "./Scratch_card"
+import seven from "./assets/777-removebg-preview.svg" 
 
 export default function Luck()
 {
     return(
         <>
-            <div className="h-80 py-12">
+            <div className="h-80 py-12" style={{ backgroundImage: `url(${seven})`, backgroundSize: 'cover' }}>
                 <ScratchCard/>
             </div>
         </>
