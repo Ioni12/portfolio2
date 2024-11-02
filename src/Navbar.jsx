3a60199e41e371b27fb27420df7fdd775b7a34ea
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-02 16:21:35 +0100

    seeing

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar()
     return(
         <>
            <div className="container mx-auto px-10 mt-14 sticky top-2 z-10">
-            <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 sticky top-0">
+            <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2  sticky top-0">
                 <Navbar_inside text="Home" to="home" />
                 <Navbar_inside text="About" to="about" />
                 <Navbar_inside text="Contact" to="info" />
