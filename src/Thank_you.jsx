567cc3bd51d3aa893bb69225184548ad96b4663a
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-02 13:36:17 +0100

    the thank you

--- src/Thank_you.jsx
+++ src/Thank_you.jsx
@@ -4,7 +4,7 @@ export default function Thank_you()
 {
     return(
         <>
-            <div className=" h-32 flex items-center justify-center">
+            <div className=" h-32 flex items-center justify-center mt-10 mb-10">
                 <h1 className="font-Tanker text-[48px]">Thank you</h1>
             </div>
         </>
