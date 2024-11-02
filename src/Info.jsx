89f70c8237713589675d4abe94c51c550ac26d6f
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-02 13:25:49 +0100

    some new details

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,4 +1,6 @@
-
+import gmail from "./assets/gmail-icon.svg"
+import linkedin from "./assets/linkedin-icon-2.svg"
+import instagram from "./assets/instagram-glyph.svg"
 
 export default function Info()
 {
@@ -6,11 +8,19 @@ export default function Info()
         <>
             <div className=" h-40">
                 <div className="flex">
-                    <div className="flex items-center mt-5 ml-5">
-                        <h2>email: enkeljongjeta@gmail.com</h2>
-                        <img src="" alt="" />
-                        <h2>linkedin</h2>
-                        <h2>instagram</h2>
+                    <div className=" mt-5 ml-5">
+                        <h2 className="flex p-4">
+                            <img src={gmail} alt="" className="w-4 h-4 mt-1 mr-1"/>email: enkeljongjeta@gmail.com
+                        </h2>
+                        <br />
+                        <h2 className="flex p-4 -mt-8">
+                            <img src={linkedin} alt="" className="w-4 h-4 mt-1 mr-1"/>linkedin:  <a href="https://www.linkedin.com/in/enkeljon-gjeta-7b137214b/">Enkeljon Gjeta</a>
+                        </h2>
+                        <br />
+                        <h2 className="flex p-4 -mt-8">
+                            <img src={instagram} alt="" className="w-4 h-4 mt-1 mr-1"/>instagram: enkeljon_gjeta
+                        </h2>
+                       
                     </div>
                 </div>
             </div>
