4739e37875d86b8e3fa082a7d30c1684b294290d
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-02 12:23:28 +0100

    working on

--- src/Comming_soon.jsx
+++ src/Comming_soon.jsx
@@ -7,10 +7,16 @@ export default function Comming_soon()
             <div className="h-[24rem] justify-center">
                 <h1 className="ml-32 mt-10 -mb-8">currently working on</h1>
                 <div className="grid grid-cols-3 gap-2 mt-28">
-                    <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 ml-4">
-                        
+                    <div className=" rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-1 sm:p-1 w-28 h-28 ml-4">
+                        <h1>react chatt app</h1>
+                        <h1>In Development</h1>
+                        <h2>React, Node js, Firebase</h2>
                     </div>
-                    <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mx-auto"></div>
+                    <div className=" rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-1 sm:p-1 w-28 h-28 mx-auto">
+                        <h1>react drag n drop</h1>
+                        <h1>In Development</h1>
+                        <h2>React, konva js</h2>
+                    </div>
                     <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mr-"></div>
                 </div>
                 
