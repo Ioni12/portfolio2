36143a4b531b52fc97630605c6530f9b434521aa
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-02 12:48:11 +0100

    try

--- src/Comming_soon.jsx
+++ src/Comming_soon.jsx
@@ -8,16 +8,20 @@ export default function Comming_soon()
                 <h1 className="ml-32 mt-10 -mb-8">currently working on</h1>
                 <div className="grid grid-cols-3 gap-2 mt-28">
                     <div className=" rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-1 sm:p-1 w-28 h-28 ml-4">
-                        <h1>react chatt app</h1>
-                        <h1>In Development</h1>
-                        <h2>React, Node js, Firebase</h2>
+                        <h1 className="text-[14px] mb-1">react chatt app</h1>
+                        <h1 className="text-[14px] mb-1">In Development </h1>
+                        <h2 className="text-[14px]">React, Node js, Firebase 🚧</h2>
                     </div>
                     <div className=" rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-1 sm:p-1 w-28 h-28 mx-auto">
-                        <h1>react drag n drop</h1>
-                        <h1>In Development</h1>
-                        <h2>React, konva js</h2>
+                        <h1 className="text-[14px]">react drag n drop</h1>
+                        <h1 className="text-[14px]">In Development</h1>
+                        <h2 className="text-[14px]">React, konva js 🚧</h2>
                     </div>
-                    <div className="flex justify-between items-center rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-2 sm:p-4 w-28 h-28 mr-"></div>
+                    <div className=" rounded-lg sm:rounded-full bg-white outline shadow-fuchsia-300 shadow-md p-1 sm:p-1 w-28 h-28 mr-">
+                        <h1 className="text-[14px] mb-2">soulsLike game</h1>
+                        <h1 className="text-[14px] mb-2">In Development</h1>
+                        <h2 className="text-[14px]">unity 🚧</h2>
+                    </div>
                 </div>
                 
             </div>
