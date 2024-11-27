ee2628dccb672416a805ace056c175dff0b72db9
enkeljon gjeta <enkelionimarku@gmail.com>
2024-11-27 17:34:57 +0100

    a mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,114 @@
-import Navbar from "./Navbar"
-import Introduction from "./Introduction"
-import Bento_ui from "./Bento_ui"
-import Slide from "./Slide"
-import Comming_soon from "./Comming_soon"
-import Luck from "./Luck"
-import Thank_you from "./Thank_you"
-import Info from "./Info"
+import { useRef } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { useRef } from "react"
+import ScrollTrigger from "gsap/ScrollTrigger";
 
+// Components
+import Navbar from "./Navbar";
+import Introduction from "./Introduction";
+import BentoUI from "./Bento_ui";
+import Slide from "./Slide";
+import ComingSoon from "./Comming_soon";
+import Luck from "./Luck";
+import ThankYou from "./Thank_you";
+import Info from "./Info";
 
+// Register GSAP plugins
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(useGSAP,ScrollTrigger);
+const App = () => {
+  const bentoRef = useRef(null);
+  const containerRef = useRef(null);
+  const slideRef = useRef(null);
 
-
-
-function App() {
-
-  const bento = useRef(null);
-
-  useGSAP(() => {
-     gsap.fromTo(
-      bento.current,
-      {
-        opacity: 0,
-        y: 100
-      },
-      {
-        opacity: 1,
-        y: 0,
-        duration: 1,
-        ease: "power2.out",
-        scrollTrigger: {
-          trigger: bento.current,
-          start: "top center",
-          end: "200px center",
-          markers: true,
-          toggleActions: "play pause reverse pause"
+  useGSAP(
+    () => {
+      // Bento animation
+      gsap.fromTo(
+        bentoRef.current,
+        {
+          opacity: 0,
+          y: 100,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1.5,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: bentoRef.current,
+            start: "-100px bottom-=100",
+            end: "top center",
+            toggleActions: "play none none reverse",
+            markers: true,
+            scrub: 1,
+          },
         }
-      }
-    );
-  }, []);
+      );
+
+      // Slide animation
+      gsap.fromTo(
+        slideRef.current,
+        {
+          opacity: 0,
+          x: -100, // Slide in from left
+        },
+        {
+          opacity: 1,
+          x: 0,
+          duration: 1.5,
+          ease: "power3.out",
+          scrollTrigger: {
+            trigger: slideRef.current,
+            start: "center bottom-=100",
+            end: "top center",
+            toggleActions: "play none none reverse",
+            markers: true,
+            scrub: 1,
+          },
+        }
+      );
+    },
+    { scope: containerRef }
+  );
 
   return (
-    <>
-      <Navbar/>
-      <section id="home"><Introduction /></section>
-      <section id="about" ><Bento_ui ref={bento}/></section>
-      <Slide/>
-      <Comming_soon/>
-      <Luck/>
-      <Thank_you/>
-      <section id="info"><Info /></section>
-    </>
-  )
-}
+    <div ref={containerRef} className="relative">
+      <Navbar />
 
-export default App
+      <section id="home" className="min-h-screen">
+        <Introduction />
+      </section>
+
+      <section id="about" className="min-h-screen">
+        <div ref={bentoRef}>
+          <BentoUI />
+        </div>
+      </section>
+
+      <section id="features" className="min-h-screen">
+        <div ref={slideRef}>
+          <Slide />
+        </div>
+      </section>
+
+      <section id="roadmap" className="min-h-screen">
+        <ComingSoon />
+      </section>
+
+      <section id="luck" className="min-h-screen">
+        <Luck />
+      </section>
+
+      <section id="thanks" className="min-h-screen">
+        <ThankYou />
+      </section>
+
+      <section id="info" className="min-h-screen">
+        <Info />
+      </section>
+    </div>
+  );
+};
+
+export default App;
--- src/Luck.jsx
+++ src/Luck.jsx
@@ -1,14 +1,15 @@
-import ScratchCard from "./Scratch_card"
+import ScratchCard from "./Scratch_card";
 import seven from "./assets/777-removebg-preview.svg";
 
-
-export default function Luck()
-{
-    return(
-        <>
-            <div className="h-80 py-12" style={{ backgroundImage: `url(${seven})`, backgroundSize: 'cover' }}>
-                <ScratchCard/>
-            </div>
-        </>
-    )
+export default function Luck() {
+  return (
+    <>
+      <div
+        className="h-80 py-12 lg:h-[30rem] lg:-mt-32"
+        style={{ backgroundImage: `url(${seven})`, backgroundSize: "cover" }}
+      >
+        <ScratchCard />
+      </div>
+    </>
+  );
 }
